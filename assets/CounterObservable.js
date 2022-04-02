dde2f9aa7fd6964c4f916fb62c0ba6f1af4f9d09
Hanskaspar Frei <hkfrei@karten-werk.ch>
2022-04-02 14:03:05 +0200

    feat: add a Observable class.

---
+++ assets/CounterObservable.js
@@ -0,0 +1,16 @@
+class CounterObservable {
+  constructor() {
+    this.observers = [];
+  }
+  subscribe(func) {
+    this.observers.push(func);
+  }
+  unsubscribe(func) {
+    this.observers = this.observers.filter((observer) => observer !== func);
+  }
+  notify(data) {
+    this.observers.forEach((observer) => observer(data));
+  }
+}
+
+export default CounterObservable;
